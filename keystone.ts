3cf264655e4adc7d95b3f67fca5f053f1849db11
Gagan Awhad <gagan.a.awhad@gmail.com>
2025-01-09 15:57:57 -0600

    Define user model

--- keystone.ts
+++ keystone.ts
@@ -1,10 +1,21 @@
 // keystone.ts
-import { config } from '@keystone-6/core';
+import { config, list } from '@keystone-6/core';
+import { allowAll } from '@keystone-6/core/access';
+import { text } from '@keystone-6/core/fields';
+
 
 export default config({
   db: {
     provider: 'sqlite',
     url: 'file:./keystone.db',
   },
-  lists: {} // ...
+  lists: {
+    User: list({
+      access: allowAll,
+      fields: {
+        name: text({ validation: { isRequired: true } }),
+        email: text({ validation: { isRequired: true }, isIndexed: 'unique' }),
+      },
+    }),
+  }
 });
